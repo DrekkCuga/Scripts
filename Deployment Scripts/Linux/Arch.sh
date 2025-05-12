fb8d476c02162ddc1bb97b85654df3ee6cf41f80
Drekk <drekk@nargacu.ga>
2025-05-12 15:31:47 +1000

    Reorganised and added linux-headers + piper

--- Deployment Scripts/Linux/Arch.sh
+++ Deployment Scripts/Linux/Arch.sh
@@ -8,9 +8,7 @@ rm -R /tmp/yay-bin
 
 #Nvidia shit
 sudo pacman -S nvidia-dkms
-yay -S nvidia-utils
-yay -S all-ways-egpu
-yay -S nvidia
+yay -S nvidia-utils nvidia all-ways-egpu
 sudo all-ways-egpu
 #TODO automate all-ways-egpu method setup
 #force it to pick amd igpu and nvidia egpu? idfk if anyone else is ever gonna use this shithole script who might want something else, fuck I hope not
@@ -21,13 +19,15 @@ sudo all-ways-egpu
 #Gaming and libraries
 sudo pacman -S lib32-vulkan-radeon lib32-nvidia-utils steam
 yay r2modman
-sudo pacman -S prismlauncher
+sudo pacman -S prismlauncher piper
+
 
 #Other shit
-sudo pacman -S obsidian
-sudo pacman -S vscode
-yay -S spotify
-yay -S pinta
+sudo pacman -S obsidian vscode
+yay -S spotify pinta
+
+#Linux shit
+sudo pacman -S linux-headers
 
 #Tailscale
 sudo pacman -S tailscale
