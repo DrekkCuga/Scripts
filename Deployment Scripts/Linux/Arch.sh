c4b8d753e51b529320e6b7788b3ba9e9fd1da083
Drekk <drekk@nargacu.ga>
2025-05-13 19:58:03 +1000

    Updated from nvidia to nvidia-open

--- Deployment Scripts/Linux/Arch.sh
+++ Deployment Scripts/Linux/Arch.sh
@@ -7,8 +7,8 @@ makepkg -si
 rm -R /tmp/yay-bin
 
 #Nvidia shit
-sudo pacman -S nvidia-dkms
-yay -S nvidia-utils nvidia all-ways-egpu
+sudo pacman -S nvidia-open-dkms nvidia-open nvidia-utils
+yay -S all-ways-egpu
 sudo all-ways-egpu
 #TODO automate all-ways-egpu method setup
 #force it to pick amd igpu and nvidia egpu? idfk if anyone else is ever gonna use this shithole script who might want something else, fuck I hope not
